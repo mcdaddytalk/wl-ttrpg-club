3998eb33237a23c00ab9f131b194310ea7d4555c
Karlan Talkington <kbtalkin@gmail.com>
2024-11-13 00:23:55 -0500

    bug fix in email resend

--- src/app/api/email/new-contact/route.ts
+++ src/app/api/email/new-contact/route.ts
@@ -8,18 +8,20 @@ export async function POST(request: Request): Promise<NextResponse> {
   const contactData = await request.json();
   try {
     const { error } = await resend.emails.send({
-      from: 'WL-TTRPG <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      from: 'WL-TTRPG <onboarding@kaje.org>',
+      to: ['kbtalkin+newcontact+wlttrpg@gmail.com'],
       subject: 'New Contact Form Submission',
       react: NewContactEmail({ ...contactData }),
     });
 
     if (error) {
+      console.error(error);
       return NextResponse.json({ error }, { status: 500 });
     }
 
     return NextResponse.json({ status: 200 });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
