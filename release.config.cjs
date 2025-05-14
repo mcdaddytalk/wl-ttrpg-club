b1b9d83b7aecf3de758935a27c1e298b0653223f
MCDaddyTalk <kbtalkin@gmail.com>
2025-05-14 15:37:52 -0400

    chore(minor): GitHub action semantic release update (#92)
    * chore(minor): refurbish actions with semantic release
    
    * chore(minor): refurbish actions with semantic release
    
    * chore(minor): update semantic-release and supporting files
    
    * fix(minor):  forgot to update lock file

--- release.config.cjs
+++ release.config.cjs
@@ -1,12 +1,18 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/github',
-      '@semantic-release/git'
-    ],
+    '@semantic-release/commit-analyzer',       // determines version bump (based on commit messages)
+    '@semantic-release/release-notes-generator', // generates changelog content
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+      changelogTitle: '# 📦 Changelog'
+    }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    '@semantic-release/github'
+  ],
     verifyConditions: [
       '@semantic-release/github',
       [
